a0d93a6234e4c57454ba631286a6e9509b59703b
Justin <6057303+JSArrakis@users.noreply.github.com>
2024-01-02 13:37:29 -0800

    Adding loading of content from different pages

--- renderer.js
+++ renderer.js
@@ -1,8 +1,22 @@
 const { ipcRenderer } = require('electron');
+const fs = require('fs').promises;
 
-document.getElementById('myButton').addEventListener('click', function () {
-    // Send an IPC message to the main process
-    console.log('Sending message to main process');
+document.getElementById('myButton').addEventListener('click', async function () {
+    let filePath = './test.html';
+    console.log('Loading content...');
+    try {
+        // Use await with fs.promises.readFile to read the file content
+        let fileContent = await fs.readFile(filePath, 'utf-8');
+
+        // Update the main-content div with the loaded content
+        document.getElementById('fileContent').innerHTML = fileContent;
+
+        // Log a message to the console
+        console.log('Content loaded');
+    } catch (err) {
+        console.error(err);
+    }
+
 });
 
 document.getElementById('openFileButton').addEventListener('click', function () {
